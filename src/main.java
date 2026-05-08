8c502be9142b69d02dac1cd7238791454817d8f3
I3R1T3P11 <sekaachi8@gmail.com>
2026-05-08 16:45:58 +0000

    commit fin

--- src/main.java
+++ src/main.java
@@ -1,12 +1,13 @@
-public class main {
-
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Personne personne1 = new Personne();
         Personne personne2 = new Personne();
         Personne personne3 = new Personne();
         Personne personne4 = new Personne();
-        System.out.println("(" + personne4.nbLocal + "," +
-                Personne.nbInstances + ")");
+
+        // Affichage des compteurs
+        System.out.println("personne1: (" + personne1.nbLocal + "," + Personne.nbInstances + ")");
+        System.out.println("personne2: (" + personne2.nbLocal + "," + Personne.nbInstances + ")");
+        System.out.println("personne3: (" + personne3.nbLocal + "," + Personne.nbInstances + ")");
+        System.out.println("personne4: (" + personne4.nbLocal + "," + Personne.nbInstances + ")");
     }
 
-}
