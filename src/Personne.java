0ebfc7e85c028e95d87dc0bd610d007f0bc80c5e
I3R1T3P11 <sekaachi8@gmail.com>
2026-05-08 16:39:27 +0000

    commit implementation

--- src/Personne.java
+++ src/Personne.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Personne {
+
+    public static int nbInstances;
+    public int nbLocal;
+    public Personne(){
+        nbInstances++;
+        nbLocal++;
+    }
 }
